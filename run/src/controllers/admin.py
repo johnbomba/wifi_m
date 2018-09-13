d5aa1b86bc76cbc9598baa92f288a201b4356c93
John Bomba <bomba.john@gmail.com>
2018-09-13 12:04:41 -0400

    sudo code for admin.py and admin.html

---
+++ run/src/controllers/admin.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+
+# pseudocode
+# primary function 
+
+# function to start/ stop miner 
+
+# function to track number of users
+
+# funtion to display total hash rate
+
+# secondary functions
+
+# function to display individual hash rate 
+
+
