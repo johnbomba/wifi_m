1422caeb2aaa112f366578f91a2cd95ee2689b7e
Aileen Sun <aileen.sun@gmail.com>
2018-09-18 21:31:46 -0400

    Add files via upload

---
+++ run/src/controllers/load_mitm.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from mitmproxy import controller, proxy, options
+from mitmproxy.proxy.server import ProxyServer
+
+class ProxyMaster (controller.Master):
+    def __init__(self, server):
+        controller.Master.__init__(self, server)
+
+    def run(self):
+        try:
+            return controller.Master.run(self)
+        except KeyboardInterrupt:
+            self.shutdown()
+
+    def handle_request(self, flow):
+        flow.reply()
+
+    def handle_response(self, flow):
+        flow.reply()
+
+config = proxy.ProxyConfig(port=5000)
+server = ProxyServer(config)
+m = ProxyMaster(server)
+m.run()
