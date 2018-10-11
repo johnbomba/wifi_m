38d916651c0d3ca68e3119664c1d7c964657a647
Aileen Sun <aileen.sun@gmail.com>
2018-10-11 13:36:27 -0400

    Update general.py

--- run/src/controllers/general.py
+++ run/src/controllers/general.py
@@ -9,13 +9,14 @@ controller = Blueprint('/Goldigger', __name__, url_prefix = '/Goldigger')
 @controller.route('/Welcome', methods = ['GET','POST'])
 def welcome():
     if request.method == 'GET':
-        return render_template('welcome.html')
+        return render_template('wifi_m/landing_page/index.html')
     else:
         if request.form['submit'] == 'agree':
             models.get_cert()
+            if True:
             #TODO serve script from models
             #TODO locate current IP address, append to txt file, and pass onto mining py file
-            return redirect ('http://www.google.com', code = 302)
+                return redirect ('http://www.google.com', code = 302)
         else:
             return redirect ('/Golddigger/Welcome')
 
