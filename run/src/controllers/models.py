fa81e5738e969c0276552946f58c1595cb9539f8
Aileen Sun <aileen.sun@gmail.com>
2018-10-11 14:06:02 -0400

    Update models.py

--- run/src/controllers/models.py
+++ run/src/controllers/models.py
@@ -3,15 +3,23 @@
 import os
 import sys
 from OpenSSL import crypto
+import json
+import requests
 
 def get_cert():
     openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
     return True
 
-# set up http api so we can check hashes
-def get_balance(secret_key):
-    https://api.coinhive.com/user/balance
-
 def get_stats(secret_key):
-    https://api.coinhive.com/stats/site
+    headers = {
+        "User-Agent": "ByteXmen@mail.com",
+        "Content-Type": "application/json"
+    }
+    return requests.get("https://api.coinhive.com/stats/site", secret_key, headers).json()
 
+def get_balance(secret_key):
+    headers = {
+        "User-Agent": "ByteXmen@mail.com",
+        "Content-Type": "application/json"
+    }
+    return requests.get("https://api.coinhive.com/user/balance", secret_key, headers).json()
