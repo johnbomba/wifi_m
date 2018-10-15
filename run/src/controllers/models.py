53d7d8c7e2d36a5aaaa9a2a182e7879ff6c84c87
Aileen Sun <aileen.sun@gmail.com>
2018-10-15 13:31:49 -0400

    Update models.py

--- run/src/controllers/models.py
+++ run/src/controllers/models.py
@@ -8,9 +8,13 @@ import sys
 def start_mitm():
     pass
     
+def configure_iptables():
+    os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
+    return True
+    
 def get_cert(path, basename):
     os.system("~/.local/bin/mitmproxy -p 8080 -s 'cert *=cert.pem' 'controllers/injector.py controllers/script.js' -T")
-    pass
+    return True
 
 def get_stats(secret_key, params={}):
     headers = {
